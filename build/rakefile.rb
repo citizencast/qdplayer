c6cc6684fb9f6941452a900c4d5d81c9253ecd7c
Yvon <yvon.cognard@gmail.com>
2009-03-19 11:53:05 +0100

    "Release" rake task

--- build/rakefile.rb
+++ build/rakefile.rb
@@ -1,11 +1,22 @@
 namespace :flash do
-  desc "compile"
-  task(:compile) do
+  desc "compile a debug version"
+  task(:debug) do
     raise unless system <<-CMD \
       mxmlc ../src/flowersway.mxml \
         -output public/player.swf \
         -warnings \
         -compiler.debug \
+        -strict \
+        -target-player 9.0
+    CMD
+  end
+  
+  desc "compile a release version"
+  task(:release) do
+    raise unless system <<-CMD \
+      mxmlc ../src/flowersway.mxml \
+        -output release.swf \
+        -warnings \
         -strict \
         -target-player 9.0
     CMD
