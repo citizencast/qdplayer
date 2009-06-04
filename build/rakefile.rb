e6fc0d89d24b9fb4f75325a32135214d4c485706
Yvon <yvon.cognard@gmail.com>
2009-06-04 13:01:03 +0200

    Test directory

--- build/rakefile.rb
+++ build/rakefile.rb
@@ -1,24 +1,24 @@
 namespace :flash do
   desc "compile a debug version"
   task(:debug) do
-    raise unless system <<-CMD \
-      mxmlc ../src/flowersway.mxml \
-        -output public/player.swf \
-        -warnings \
-        -compiler.debug \
-        -strict \
+    raise unless system %{
+      mxmlc ../src/flowersway.mxml
+        -output ../test/public/player.swf
+        -warnings
+        -compiler.debug
+        -strict
         -target-player 9.0
-    CMD
+    }.split.join(' ')
   end
   
   desc "compile a release version"
   task(:release) do
-    raise unless system <<-CMD \
-      mxmlc ../src/flowersway.mxml \
-        -output release.swf \
-        -warnings \
-        -strict \
+    raise unless system %{
+      mxmlc ../src/flowersway.mxml
+        -output release.swf
+        -warnings
+        -strict
         -target-player 9.0
-    CMD
+    }.split.join(' ')
   end
 end
