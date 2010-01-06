841007de7ce16a56a0c85963783ba0397f99af59
Michael Bensoussan <mike@michael-bensoussans-macbook-pro.local>
2010-01-06 05:03:43 +0100

    few changes for the player to work with flex4/flash10

--- build/rakefile.rb
+++ build/rakefile.rb
@@ -13,6 +13,8 @@ namespace :flash do
   
   desc "compile a release version"
   task(:release) do
+    #compiled it with flex4 and flash10
+    #raise unless system("/Applications/Adobe_Flash_Builder/sdks/4.0.0/bin/mxmlc ../src/flowersway.mxml -output qdplayer.swf -warnings=false -strict -optimize=true -target-player 10.0 -compatibility-version=3.0")
     raise unless system %{
       mxmlc ../src/flowersway.mxml
         -output release.swf
